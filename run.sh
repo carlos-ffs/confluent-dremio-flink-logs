6138c613c70a31c970da3cf99af9e758a20d0499
Carlos Santos <carlos.santos@dremio.com>
2025-11-25 10:29:46 +0000

    create pull secret

--- run.sh
+++ run.sh
@@ -7,4 +7,9 @@ helm dependency build ./charts/argo-cd
 helm upgrade --install --atomic argocd ./charts/argo-cd -n argocd --kubeconfig $KUBECONFIG
 
 kubectl apply -f repo.yaml -n argocd --kubeconfig $KUBECONFIG
-kubectl apply -f argocd-init.yaml -n argocd --kubeconfig $KUBECONFIG
+
+# After argocd is up and running, we should apply the image pull secret 
+# with dremio quay credentials.
+# On the other hand, if you are using an EKS, GKE, or AKS, 
+# we can leverage external-secrets to manage the pull secret seemlessly.
+kubectl apply -f pull-secret.yaml -n dremio --kubeconfig $KUBECONFIG
