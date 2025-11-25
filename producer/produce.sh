93538c14740a6868ba9cecfc5dd0542659e1ba21
Carlos Santos <carlos.santos@dremio.com>
2025-11-25 21:15:15 +0000

    install fluent bit

--- producer/produce.sh
+++ producer/produce.sh
@@ -3,7 +3,7 @@
 # Configuration
 RANDOM_MSG="msg-$(date +%s)-$RANDOM"
 TOPIC_NAME="test-hello"
-BOOTSTRAP="localhost:9092"
+BOOTSTRAP="localhost:30000"
 
 KAFKA_VERSION="4.1.1"
 SCALA_VERSION="2.13"
@@ -15,14 +15,6 @@ KAFKA_DIR="./kafka_${SCALA_VERSION}-${KAFKA_VERSION}"
 if [ ! -d "${KAFKA_DIR}" ]; then
   echo "Downloading Kafka ${KAFKA_VERSION}..."
   curl -L "$KAFKA_URL" | tar xz
-fi
-
-# Check if port-forward is running
-if ! nc -z localhost 9092 2>/dev/null; then
-  echo "ERROR: Kafka is not accessible on localhost:9092"
-  echo "Please run port-forward in another terminal:"
-  echo "  kubectl port-forward -n confluent kafka-0 9092:9092"
-  exit 1
 fi
 
 # kubectl get secret ca-pair-sslcerts -n confluent -o jsonpath='{.data.tls\.crt}' | base64 -d > producer/ca.crt
